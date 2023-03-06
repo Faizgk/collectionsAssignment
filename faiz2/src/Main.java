f82de482681c33f2933d90b7560a61a0ac3f7a26
Faizgk <gkfaiz123@gmail.com>
2023-03-06 11:39:37 +0530

    modified Q2

--- faiz2/src/Main.java
+++ faiz2/src/Main.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -6,38 +7,74 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         Database db = new Database();
+            while(true){
+                System.out.println();
+                System.out.println("> Enter 1 to View");
+                System.out.println("> Enter 2 to search");
+                System.out.println("> Enter 3 to delete element");
+                System.out.println("> Enter 4 to add element");
+                System.out.println("> Enter 5 to exit");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
 
-        while(true){
-            System.out.println();
-            System.out.println("Enter Product ID");
-            String productId = scanner.nextLine();
-            System.out.println("Enter Product Name");
-            String productName = scanner.nextLine();
+                switch(choice){
 
-            Product product = new Product(productId, productName);
+                    case 1 : db.Display();
+                        break;
+                    case 2:
 
-            db.store(product);
+                        System.out.println("Search by Key --1");
+                        System.out.println("Search by Element --2");
+                        int choice2 = scanner.nextInt();
+                        scanner.nextLine();
+                        if(choice2==2){
+                            System.out.println("Enter the element");
+                            if(db.hashtable.contains(scanner.nextLine())){
+                                System.out.println("Element Found") ;
+                            }
+                            else{
+                                System.out.println("Element Not Found");
+                            }
+                        }else {
+                            System.out.println("Enter the key");
+                              String key = scanner.nextLine();
+                            if(db.hashtable.containsKey(key)){
+                                System.out.println("Key Found having value :  "+db.hashtable.get(key));
+                            }else{
+                                System.out.println("Key not Found");
+                            }
+                        }
 
-            System.out.println();
-            System.out.println("> Enter X to View and exit");
-            System.out.println("> Enter Any Other Letter to View and Add");
-            char x = scanner.next().toLowerCase().charAt(0);
-            scanner.nextLine();
+                        break;
+                    case 3:
+                        System.out.println("Enter the id to delete");
+                        db.hashtable.remove(scanner.nextLine());
+                        System.out.println("Id and it's Element deleted");
+                        break;
+                    case 5:
+                        System.out.println("Exiting...");
+                        System.exit(0);
+                        break;
+                    case 4:
 
-            if(x == 'x'){
+                        System.out.println();
+                        System.out.println("Enter Product ID");
+                        String productId = scanner.nextLine();
+                        System.out.println("Enter Product Name");
+                        String productName = scanner.nextLine();
 
-                db.Display();
-                System.exit(0);
-            }else{
-                db.Display();
+                        Product product = new Product(productId, productName);
+                        db.store(product);
+
+
+                }
 
             }
-        }
+
 
 
 
     }
-
 
 
 }
